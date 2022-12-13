ef3716cc22cf8c962f7535e8241207437afee096
Thor Lindberg <hello@thorlindberg.com>
2022-12-13 10:20:04 +0000

    Updated day-2

--- day-2/main.swift
+++ day-2/main.swift
@@ -1,9 +1,3 @@
-import Foundation
-import model
-
 let dataModel = DataModel(jsonFilename: "data.json")
-
 let score = dataModel.score
 print(score)
-
-// missing result for model-alt.swift
--- day-2/model.swift
+++ day-2/model.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public struct DataModel {
 
   var rounds: [Round?]
